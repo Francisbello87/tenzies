b96a488913461c6883bb652754e740caaa2caa46
Francis Bello <bellofrancis87@gmail.com>
2022-01-31 14:33:34 +0100

    game is working perfectly

--- src/App.js
+++ src/App.js
@@ -1,14 +1,82 @@
 import Die from "./components/Die";
+import React from "react";
+import { nanoid } from "nanoid";
 import "./main.css";
-import data from "./data";
+import Confetti from "react-confetti";
 
 function App() {
-  const values = data.map((value) => {
-    return <Die key={value.id} {...value} />;
-  });
+  const [rollDice, setRollDice] = React.useState(allNewDice());
+  const [tenzies, setTenzies] = React.useState(false);
+
+  React.useEffect(
+    function () {
+      const allHeld = rollDice.every((die) => die.isHeld);
+      const firstValue = rollDice[0].value;
+      const allSameValue = rollDice.every((die) => die.value === firstValue);
+      if (allHeld && allSameValue) {
+        setTenzies(true);
+        console.log("You won!");
+      }
+    },
+    [rollDice]
+  );
+
+  function generateNewDie() {
+    return {
+      value: Math.ceil(Math.random() * 6),
+      isHeld: false,
+      id: nanoid(),
+    };
+  }
+
+  function allNewDice() {
+    const newDice = [];
+    for (let i = 0; i < 10; i++) {
+      newDice.push(generateNewDie());
+    }
+    return newDice;
+  }
+
+  // console.log(allNewDice());
+  const diceElements = rollDice.map((die) => (
+    <Die
+      key={die.id}
+      value={die.value}
+      isHeld={die.isHeld}
+      holdDice={() => holdDice(die.id)}
+    />
+  ));
+
+  function handleChange() {
+    if (!tenzies) {
+      setRollDice((oldDice) =>
+        oldDice.map((die) => {
+          return die.isHeld ? die : generateNewDie();
+        })
+      );
+    } else {
+      setTenzies(false);
+      setRollDice(allNewDice());
+    }
+  }
+
+  function holdDice(id) {
+    setRollDice((oldDice) =>
+      oldDice.map((die) => {
+        return die.id === id ? { ...die, isHeld: !die.isHeld } : die;
+      })
+    );
+  }
   return (
     <main>
-      <div className="die">{values}</div>
+      {tenzies && <Confetti />}
+      <h1 className="title">Tenzies</h1>
+      <p className="instructions">
+        Roll until all dice are the same. Click each die to freeze it at its
+        current value between rolls.
+      </p>
+      <div className="dice-container">{diceElements}</div>
+      <button onClick={handleChange}> {tenzies ? "New Game" : "Roll"}</button>
     </main>
   );
 }
--- src/components/Die.js
+++ src/components/Die.js
@@ -1,9 +1,12 @@
 import React from "react";
 
 function Die(props) {
+  const styles = {
+    backgroundColor: props.isHeld ? "#59E391" : "white",
+  };
   return (
-    <div className="value">
-      <span>{props.value}</span>
+    <div className="face" style={styles} onClick={props.holdDice}>
+      <h2 className="num">{props.value}</h2>
     </div>
   );
 }
